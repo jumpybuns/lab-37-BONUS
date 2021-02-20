0ae24fe5cba128779cceab7c8de06306e895632b
Jumpybuns <ethanpiercepresents@gmail.com>
2021-02-19 17:13:40 -0800

    add userReducers

--- src/reducers/userReducers.js
+++ src/reducers/userReducers.js
@@ -1,19 +1,19 @@
 import { FIND_USER } from '../actions/userActions';
 
 export const initialState = {
-  users: []
+  users: [],
 };
 
-export default userReducer(state = initialState, action) {
+export default function userReducer(state = initialState, action) {
   const { users } = state;
   switch (action.type) {
     case FIND_USER:
       return {
         ...state,
-        users: [...users, action.payload]
-      }
-      
+        users: [...users, action.payload],
+      };
+
     default:
-      return state
+      return state;
   }
 }
